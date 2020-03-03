b5cea714d838f9a1a572ebef73c57bcc9310b31d
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-03-03 12:00:18 -0800

    added jsx project

---
+++ react/first-react-with-jsx/my_first_react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+import HelloWorld from './components/MyNewComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/first-react-with-jsx/my_first_react/src/components/MyNewComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <h1>
+                Hello World
+            </h1>
+        )
+    }
+}
+export default MyNewComponent;
