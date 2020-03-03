c91956347fc75a4113ffe8e8b5168428758e9e91
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-03-03 12:57:24 -0800

    adding prop practices

---
+++ react/prop-it-up/prop_it_up/src/components/MyNewComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class MyNewComponent extends Component{
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return(
+            <div>
+                <h1>{this.props.lastName}, {this.props.firstName} </h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+            </div>
+        )
+    }
+}
+export default MyNewComponent;
