187226bc59b49c95fc757cb1aac6ba36a308b1d4
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-03-05 23:44:15 -0800

    fixed skywalker api

--- react/luke-api-walker/luke_api_walker/src/components/Person.jsx
+++ react/luke-api-walker/luke_api_walker/src/components/Person.jsx
@@ -5,12 +5,21 @@ import axios from 'axios';
 
 const Person = (props) => {
     const [state, setState] = useState({})
+    const [home, setHome]  = useState({})
     useEffect(() => {
         axios.get(`https://swapi.co/api${props.uri}`)
         .then(response => {
             setState(response.data)
+            console.log(response.data.homeworld)
+            return axios.get(response.data.homeworld)
         })
-        .catch(error => navigate('/error'))
+        .then(response => {
+            setHome(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+            return navigate('/error')
+        })
     },[props.uri])
     return (
         <div>
@@ -19,6 +28,7 @@ const Person = (props) => {
             <p>Hair Color: {state.hair_color}</p>
             <p>Eye Color: {state.eye_color}</p>
             <p>Skin Color: {state.skin_color}</p>
+            <p>Homeworld: <a href={state.homeworld} target='_blank'>{home.name}</a></p>
         </div>
     )
 }
