34aa9cf6999b1b640f9d0c949e824fca57f6094e
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-03-08 21:51:20 -0700

    added

--- mongodb/jakes-api/jokes_api/server/config/config.js
+++ mongodb/jakes-api/jokes_api/server/config/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/jokesDB',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=> console.log('Established a conniection to the database'))
+}).then(()=> console.log('Established a connection to the database'))
 .catch(err=> console.log('Something went wrong when connectiong to the database', err));
 
 
